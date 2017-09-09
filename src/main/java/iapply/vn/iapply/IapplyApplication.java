dccec94892f14f77a88d3c121a65d3d1c8a78765
loc huynh <lochp.hcmc@gmail.com>
2017-09-09 20:32:53 +0700

    create source springboot/maven

---
+++ src/main/java/iapply/vn/iapply/IapplyApplication.java
@@ -0,0 +1,12 @@
+package iapply.vn.iapply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IapplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IapplyApplication.class, args);
+	}
+}
---
+++ src/test/java/iapply/vn/iapply/IapplyApplicationTests.java
@@ -0,0 +1,16 @@
+package iapply.vn.iapply;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IapplyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
